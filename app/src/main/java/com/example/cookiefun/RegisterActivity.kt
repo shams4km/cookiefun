10efd8e0c8856e1131e5a236f6ffd366e8820bad
ktsaur <taryshkinakv@gmail.com>
2024-07-06 22:53:52 +0300

    authorization done

--- app/src/main/java/com/example/cookiefun/RegisterActivity.kt
+++ app/src/main/java/com/example/cookiefun/RegisterActivity.kt
@@ -1,5 +1,4 @@
 package com.example.cookiefun
-
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -11,47 +10,46 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class AuthActivity : AppCompatActivity() {
+
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_auth)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val userLogin: EditText = findViewById(R.id.user_login_auth)
-        val userPassword: EditText = findViewById(R.id.user_password_auth)
-        val button: Button = findViewById(R.id.button_auth)
-        //val linkToReg: TextView = findViewById(R.id.link_to_reg)
+        val userLogin: EditText = findViewById(R.id.user_login)
+        val userPassword: EditText = findViewById(R.id.user_password)
+        val userMail: EditText = findViewById(R.id.user_mail)
+        val button: Button = findViewById(R.id.button_reg)
+        //val linkToAuth: TextView = findViewById(R.id.link_to_auth)
+
+//        linkToAuth.setOnClickListener{
+//            val intent = Intent(this, AuthActivity::class.java)
+//            startActivity(intent)
+//        }
 
         button.setOnClickListener{
             val login = userLogin.text.toString().trim()
             val password = userPassword.text.toString().trim()
+            val mail = userMail.text.toString().trim()
 
-            if (login == "" || password == "") {
+            if (login == "" || password == "" || mail == "") {
                 Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
             } else {
+                val user = User(login, password, mail)
 
                 val db = DbHelper(this, null)
-                val isAuth = db.getUser(login, password)
+                db.addUser(user)
+                Toast.makeText(this, "Пользователь $login добавлен", Toast.LENGTH_LONG).show()
 
-                if (isAuth) {
-                    Toast.makeText(this, "Пользователь $login авторизован", Toast.LENGTH_LONG).show()
-
-                    userLogin.text.clear()
-                    userPassword.text.clear()
-                }else {
-                    Toast.makeText(this, "Пользователь $login НЕ авторизован", Toast.LENGTH_LONG).show()
-                }
+                userLogin.text.clear()
+                userPassword.text.clear()
+                userMail.text.clear()
             }
         }
-
-//        linkToReg.setOnClickListener{
-//            val intent = Intent(this, RegisterActivity::class.java)
-//            startActivity(intent)
-//        }
-
     }
 }
