c96aadf8357f43a2aa65aceea5ed2a6aa4d46220
KamilSab <kamilsab46@gmail.com>
2024-07-07 05:55:14 +0300

    recyclerview started

---
+++ app/src/main/java/com/example/cookiefun/rvrecipes/RecipeHolder.kt
@@ -0,0 +1,22 @@
+package com.example.cookiefun.rvrecipes
+
+import android.util.Printer
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import coil.load
+import com.example.cookiefun.databinding.ItemRecipeBinding
+
+class RecipeHolder(
+    private val binding: ItemRecipeBinding,
+    private val onClicked: (Recipe) -> Unit
+): ViewHolder(binding.root) {
+
+    fun onBind(recipe: Recipe) {
+        binding.run {
+            ivRecipeImage.load(recipe.url)
+            tvRecipeName.text = recipe.name
+            root.setOnClickListener {
+                onClicked.invoke(recipe)
+            }
+        }
+    }
+}
