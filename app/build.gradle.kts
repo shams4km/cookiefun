3539e9f0fa114d77041950488862402d01da44d5
shams4km <shams4km@gmail.com>
2024-07-07 05:18:08 +0300

    added shadows

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
-
+    implementation (libs.xmaterial)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
