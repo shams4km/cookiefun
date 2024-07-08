663124bebedb429f2d1fd6276e9b87f8c409fb6d
Ilnar21 <valiullinilnar2005@gmail.com>
2024-07-08 22:45:06 +0300

    added the implementation of hiding menus in categories

--- app/src/main/java/com/example/cookiefun/HomeActivity.kt
+++ app/src/main/java/com/example/cookiefun/HomeActivity.kt
@@ -14,9 +14,9 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val navHostFragment = supportFragmentManager
+        val navHostFragment1 = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        val navController = navHostFragment1.navController
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigationView.setupWithNavController(navController)
