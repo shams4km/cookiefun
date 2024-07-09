c456da5d2f2a760109e8662e0613ecb139619276
makarjaan <arina.makarova.08@inbox.ru>
2024-07-10 02:11:20 +0300

    the classes of the detailed recipe and the main page have been fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     val nav_version = "2.7.7"
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
